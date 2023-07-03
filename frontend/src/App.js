1311518be3aa70f44a2a6e18184df07669ee2f69
aelozen <alexgross95@gmail.com>
2023-07-03 13:20:52 -0400

    open in new window

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,6 @@ import { Login } from "./pages/Login";
 import { UserData } from "./pages/UserData";
 import { Balance } from "./pages/Balance.jsx";
 import { Transfer } from "./pages/Transfer.jsx";
-// import { Footer } from "./components/partials/footer.jsx";
 import { MDBFooter, MDBContainer, MDBBtn} from 'mdb-react-ui-kit';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faYoutube, faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
@@ -36,41 +35,47 @@ function App() {
                 <MDBFooter className='bg-light text-center text-white'>
                 <div className='text-center p-3' style={{ backgroundColor: 'seashell', color:'black' }}>
         Linked below are my Youtube tutorial, LinkedIn, and Github Repository for this project. 
-        {/* <a className='text-white' href='https://mdbootstrap.com/'>
-          MDBootstrap.com remember to add opening in another page
-        </a> */}
       </div>
       <MDBContainer className='p-4 pb-0'>
         <section className='mb-4'>
-          <MDBBtn
-            floating
-            className='m-1'
-            style={{ backgroundColor: 'red' }}
-            href='#!'
-            role='button'
-          >
-        <FontAwesomeIcon icon={faYoutube} />
-          </MDBBtn>
+        <MDBBtn
+          floating
+          className="m-1"
+          style={{ backgroundColor: 'red' }}
+          href="#!"
+          role="button"
+          disableRipple
+        >
+          <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faYoutube} />
+          </a>
+        </MDBBtn>
 
-          <MDBBtn
-            floating
-            className='m-1'
-            style={{ backgroundColor: '#0082ca' }}
-            href='https://www.linkedin.com/in/alexandra-gross-612702274/'
-            role='button'
-          >
-    <FontAwesomeIcon icon={faLinkedinIn} />
-          </MDBBtn>
+        <MDBBtn
+          floating
+          className="m-1"
+          style={{ backgroundColor: '#0082ca' }}
+          href="https://www.linkedin.com/in/alexandra-gross-612702274/"
+          role="button"
+          disableRipple
+        >
+          <a href="https://www.linkedin.com/in/alexandra-gross-612702274/" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faLinkedinIn} />
+          </a>
+        </MDBBtn>
 
-          <MDBBtn
-            floating
-            className='m-1'
-            style={{ backgroundColor: '#333333' }}
-            href='https://github.com/aelozen/better-bank'
-            role='button'
-          >
-         <FontAwesomeIcon icon={faGithub} />
-          </MDBBtn>
+        <MDBBtn
+          floating
+          className="m-1"
+          style={{ backgroundColor: '#333333' }}
+          href="https://github.com/aelozen/better-bank"
+          role="button"
+          disableRipple
+        >
+          <a href="https://github.com/aelozen/better-bank" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+        </MDBBtn>
         </section>
       </MDBContainer>
 
