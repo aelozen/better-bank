070c5489b720144b280a1a40714ff01b103b6f32
aelozen <alexgross95@gmail.com>
2023-07-03 18:01:31 -0400

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,13 @@
 const path = require("path");
 const express = require("express");
-const cors = require("cors");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
-const mongoose = require("mongoose");
 
 connectDB();
 
 const app = express();
-
-//Enable CORS for all routes
-app.use(
-    cors({
-    })
-  );
-  
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -24,13 +15,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/users", require("./routes/userRoutes"));
 
 // Serve frontend
-console.log(`=== In ${process.env.NODE_ENV} status ===`.red.underline.bold);
+console.log(`In server.js ${process.env.NODE_ENV} =+=+=+=`.red.underline.bold);
 if (
     process.env.NODE_ENV === "production" ||
     process.env.NODE_ENV === "development"
 ) {
     app.use(express.static(path.join(__dirname, "../frontend/build")));
-    mongoose.connect(process.env.MONGO_URI)
     app.get("*", (req, res) => {
         res.sendFile(
             path.resolve(__dirname, "../", "frontend", "build", "index.html")
